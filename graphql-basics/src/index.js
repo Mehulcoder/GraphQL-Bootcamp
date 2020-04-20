a933e99fedacec7e95660034574bf3c71b0b0f09
Mehulcoder <mehul355180@gmail.com>
2020-04-20 17:15:00 +0530

    Adding filters to the queries

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,19 +1,45 @@
 import {GraphQLServer} from 'graphql-yoga';
 
-//String, Boolean, int, float, ID
+//Demo user data
+var users = [{
+    id: '1',
+    name: "Mehul",
+    age: 20,
+    email: "mehul355180@gmail.com"
+},{
+    id: '2',
+    name: "Sarthak",
+    age: 17,
+    email: "sarthak355180@gmail.com"
+}]
+
+//Demo posts data
+var posts = [{
+    id:'1',
+    title:"Post1",
+    body: "Hello this is the post 1",
+    published: 0
+},{
+    id:'2',
+    title:"Post2",
+    body: "Hello this is the post 2",
+    published: 0
+}]
 
 //Type definition (schema)
 const typeDefs = `
     type Query {
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
         me: User!
         post: Post!
     }
 
-      
     type User{
         id: ID!
         name: String!
         age: Int
+        email: String!
     }
 
     type Post{
@@ -28,6 +54,28 @@ const typeDefs = `
 //Resolvers
 const resolvers = {
     Query:{
+        posts(parent, args, ctx, info){
+            if(!args.query){
+                return posts;
+            }else{
+                return posts.filter((post) => {
+                    return (post.title.toLowerCase().includes(args.query.toLowerCase())
+                    || post.body.toLowerCase().includes(args.query.toLowerCase()));
+                })
+            }
+        },
+
+        users(parent, args, ctx, info){
+            if (!args.query) {
+                return users;
+            }
+
+            return users.filter((user) => {
+                return user.name.toLowerCase().includes(args.query.toLowerCase());
+            }
+            )
+        },
+
         me(){
             return {
                 id: 'Mehul128',
