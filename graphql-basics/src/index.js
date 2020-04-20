f5b14329ec8178dfe07aef82beb61379353af63a
Mehulcoder <mehul355180@gmail.com>
2020-04-20 14:42:58 +0530

    First query

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,3 +1,28 @@
-import {message} from './myModule'
+import {GraphQLServer} from 'graphql-yoga';
 
-console.log(message);
+//Type definition (schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+
+` 
+
+//Resolvers
+const resolvers = {
+    Query:{
+        hello(){
+            return "This is my first query";
+        }
+    }
+}
+
+//Setting up server
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log("The server is up and working!");
+})
--- graphql-basics/src/myModule.js
+++
@@ -1,3 +0,0 @@
-var message  = "Some message that I'm going to export";
-
-export {message};
